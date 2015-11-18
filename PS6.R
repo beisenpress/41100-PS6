385ec81c81f59d4ee78e7aa7bcbe3f69f2199a74
beisenpress <ben@eisenpress.com>
2015-11-18 01:58:22 -0600

    Added answers to remainder of part 1, excluding MSE of lasso and Ridge

--- PS6.R
+++ PS6.R
@@ -46,21 +46,108 @@ crime.test <- crime.1[-samples,]
 null <- lm(LogViolentCR ~ 1, data=crime.train)
 
 # Create a full model with all the main effects. 
-# Remove the non-logged version of Violent CR as well as the community name varialbe
-full <- lm(LogViolentCR ~ . - communityname - ViolentCR, data=crime.train)
+full <- lm(LogViolentCR ~ ., data=crime.train)
 
 # Create a model using forward stepwise selection
 crime.reg.AIC1 <- step(null, scope=formula(full), direction="forward", k=2)
 crime.reg.BIC1 <- step(null, scope=formula(full), direction="forward", k=log(nrow(crime.train)))
-
 
 
 # (b) Redo (a) allowing for all possible interactions. What has changed?
 
 # Create a full model with all the main effects and interactions
-# Remove the non-logged version of Violent CR as well as the community name varialbe
-# full <- lm(LogViolentCR ~ . + .^2, data=crime.train)
+full2 <- lm(LogViolentCR ~ . + .^2, data=crime.train)
 
 # Create a model using forward stepwise selection
-crime.reg.AIC1 <- step(null, scope=formula(full), direction="forward", k=2)
-crime.reg.BIC1 <- step(null, scope=formula(full), direction="forward", k=log(nrow(crime.train)))
+crime.reg.AIC2 <- step(null, scope=formula(full2), direction="forward", k=2)
+crime.reg.BIC2 <- step(null, scope=formula(full2), direction="forward", k=log(nrow(crime.train)))
+
+# (c) Still within the training data, use the LASSO to select a model from all main effects and
+# interactions.
+
+# install.packages("glmnet") #just run this once (on each computer)
+library(glmnet)
+
+# Create a training dataset without the independent variable
+crime.train.2 <- crime.train[,c(-26)]
+
+X <- model.matrix(~.^2, crime.train.2) 
+#need to subtract the intercept
+X <- X[,-1]
+
+cvfit <- cv.glmnet(x = X, y = crime.train$LogViolentCR, family="gaussian", alpha=1, standardize=FALSE)
+
+betas <- coef(cvfit, s = "lambda.1se")
+model <- which(betas[2:length(betas)]!=0)
+#we need to EXCLUDE the intercept, because glmnet and lm() both put one in automatically
+
+
+#re-run regular linear model with the selected variables
+crime.reg.lasso <- lm(crime.train$LogViolentCR ~ X[,model])
+
+summary(crime.reg.lasso)
+
+
+# (d) Compute the BIC-based model probabilities for all the models found thus far, and the 
+# model which includes all main effects. Plot the fit for each model against the true value of 
+# log crime rate.
+
+# Put the BIC values for all the regressions into a vector
+# The syntax c(A=1,B=2) will return a list of named numbers (i.e. 1 is named A, 2 is named B)
+
+BIC <- c(crime.reg.AIC1=extractAIC(crime.reg.AIC1, k=log(nrow(crime.train)))[2],
+         crime.reg.BIC1=extractAIC(crime.reg.BIC1, k=log(nrow(crime.train)))[2],
+         crime.reg.AIC2=extractAIC(crime.reg.AIC2, k=log(nrow(crime.train)))[2],
+         crime.reg.BIC2=extractAIC(crime.reg.BIC2, k=log(nrow(crime.train)))[2],
+         crime.reg.lasso=extractAIC(crime.reg.lasso, k=log(nrow(crime.train)))[2])
+BIC
+
+# Apply the formula e^((-1/2)*BIC) to each element of the array. 
+eBIC <- exp(-0.5*(BIC-min(BIC)))
+eBIC
+
+# Calculate the probabliliy by dividing each eBIC by the sum of all eBIC values
+probs <- eBIC/sum(eBIC)
+round(probs, 5)
+
+# BIC says the regression from BIC forward stepwise selection has a 99% change of being right
+
+# (e) Use the test data to compare out-of-sample MSE performance. Compare your results with what 
+# you found in (d).
+
+# Calculate errors using the predict function
+crime.error.AIC1 <- predict(crime.reg.AIC1, newdata=crime.test)-crime.test$LogViolentCR
+crime.error.BIC1 <- predict(crime.reg.BIC1, newdata=crime.test)-crime.test$LogViolentCR
+crime.error.AIC2 <- predict(crime.reg.AIC2, newdata=crime.test)-crime.test$LogViolentCR
+crime.error.BIC2 <- predict(crime.reg.BIC2, newdata=crime.test)-crime.test$LogViolentCR
+#crime.error.lasso <- predict(crime.reg.lasso, newdata=crime.test)-crime.test$LogViolentCR
+
+
+#comparing the errors
+c(crime.mse.AIC1=mean(crime.error.AIC1^2), crime.mse.BIC1=mean(crime.error.BIC1^2), 
+  crime.mse.AIC2=mean(crime.error.AIC2^2), crime.mse.BIC2=mean(crime.error.BIC2^2), 
+  crime.mse.lasso=mean(crime.error.lasso^2))
+
+# The regression using AIC on interactions has the lowest MSE.
+
+# (f) Use cv.glmnet to fit Ridge regression on all main effects and interactions. Compute the 
+# out- of-sample MSE and compare to (e). (You may want to consult ?predict.cv.glmnet.)
+
+# Create a training dataset without the independent variable
+crime.train.2 <- crime.train[,c(-26)]
+
+X <- model.matrix(~.^2, crime.train.2) 
+#need to subtract the intercept
+X <- X[,-1]
+
+cvfit2 <- cv.glmnet(x = X, y = crime.train$LogViolentCR, family="gaussian", alpha=0, standardize=FALSE)
+
+betas2 <- coef(cvfit2, s = "lambda.1se")
+model2 <- which(betas2[2:length(betas2)]!=0)
+#we need to EXCLUDE the intercept, because glmnet and lm() both put one in automatically
+
+
+#re-run regular linear model with the selected variables
+crime.reg.ridge <- lm(crime.train$LogViolentCR ~ X[,model])
+
+summary(crime.reg.ridge)
